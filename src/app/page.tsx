1f9bb41ed70eb018dbe1c9ae0249cdc4137cde00
Cameron Dunn <cameron634@gmail.com>
2026-05-06 16:13:29 -0400

    add upload button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import styles from "./home.module.scss";
+import Image from 'next/image';
+import UploadIcon from "@assets/icons/upload.svg";
 
 export default function Home() {
 
     return (
         <div className={styles.home}>
-
+            <button className={styles.uploadButton}>
+                <Image src={UploadIcon} alt="Upload Icon" width={24} height={24} />
+                Upload Excel Files
+            </button>
         </div>
     )
 }
